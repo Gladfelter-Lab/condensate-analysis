2437ceef64f0e9a9a679d8c3abe95ce80478ef4c
dinosaur <alex.w.crocker@gmail.com>
2024-08-09 19:12:51 -0400

    restructured. Added notebook for interactive development

---
+++ src/condensate_analysis/_init_.py
@@ -0,0 +1,7 @@
+"""condensate-analyzer: A Python library for analyzing images of condesates."""
+
+__authors__ = "Alex Crocker, Grace McLaughlin, Vita Zhang, Sierra Cole, Benjamin Stormo, Zachary Geisterfer, Ameya Jalihal"
+
+from .read import read_image
+from .mask import mask_image
+from .featurize import featurize
