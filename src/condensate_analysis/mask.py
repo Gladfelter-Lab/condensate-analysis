089fb16c95e2e37331628d2c07f96b3b0dac4c03
dinosaur <alex.w.crocker@gmail.com>
2024-08-09 20:12:29 -0400

    mask now works for some cases

--- src/condensate_analysis/mask.py
+++ src/condensate_analysis/mask.py
@@ -1,3 +1,12 @@
-def mask_image(img):
-    # code here
+from skimage.filters import threshold_otsu
+from skimage.segmentation import clear_border
+from skimage.measure import label
+
+def mask_image(img, threshold_method="otzu", watershed=True):
+    if threshold_method == "otzu":
+        thresh = threshold_otsu(img)
+    binary = img > thresh
+    no_edge_binary = clear_border(binary)
+    label_image = label(no_edge_binary)
+    mask = label_image
     return mask
