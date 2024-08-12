1c6d59473781580cec6b24aa5422668a79f4c994
dinosaur <alex.w.crocker@gmail.com>
2024-08-12 13:47:03 -0400

    mask runs now with watershed

--- src/condensate_analysis/mask.py
+++ src/condensate_analysis/mask.py
@@ -1,11 +1,22 @@
 from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
+from skimage import segmentation
 from skimage.measure import label
+from scipy import ndimage as ndi
+from skimage.feature import peak_local_max
+import numpy as np
+# Aim to reduce variety of dependencies imported
 
-def mask_image(img, threshold_method="otzu", watershed=True):
+# Add option to specify whether to use FULLSTACK, BRIGHTEST, MAXPROJ if stack is provided. 
+# Write method for each.
+
+# Add option for background subtraction.
+# Consult Wilton on whether to use single value or also allow other method to be specified. 
+
+
+def mask_image(img, threshold_method="otzu", watershed=False):
     thresh = apply_threshold(img, threshold_method)
     binary = img > thresh
-    no_edge_binary = clear_border(binary)
+    no_edge_binary = segmentation.clear_border(binary)
     label_image = label(no_edge_binary)
     if watershed == True:
         mask = apply_watershed(binary)
@@ -21,10 +32,11 @@ def apply_threshold(img, method):
 def apply_watershed(binary):
     distance = ndi.distance_transform_edt(binary)
     distance = ndi.gaussian_filter(distance,5)
+    # Consider allowing user defined watershed distance. watershed=value accepted as well as False (none) and True (auto)
     coords = peak_local_max(distance, footprint=np.ones((25, 25)), labels=binary)
     mask = np.zeros(distance.shape, dtype=bool)
     mask[tuple(coords.T)] = True
     markers, _ = ndi.label(mask)
-    labels = watershed(-distance, markers, mask=binary)
+    labels = segmentation.watershed(-distance, markers, mask=binary)
     mask = label
-    return label_watershed
+    return labels
