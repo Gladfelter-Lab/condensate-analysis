b508ca8d07257fa36e2b5637d1c41abc4dc49f4f
Vita Zhang <vita.zhang@duke.edu>
2024-08-13 13:49:00 -0400

    add author

--- src/condensate_analysis/__init__.py
+++ src/condensate_analysis/__init__.py
@@ -1,6 +1,6 @@
 """condensate-analyzer: A Python library for analyzing images of condesates."""
 
-__authors__ = "Alex Crocker, Grace McLaughlin, Vita Zhang, Wilton Snead,Sierra Cole, Benjamin Stormo, Zachary Geisterfer, Ameya Jalihal"
+__authors__ = "Alex Crocker, Grace McLaughlin, Vita Zhang, Wilton Snead, Sierra Cole, Benjamin Stormo, Zachary Geisterfer, Ameya Jalihal"
 
 from .read import read_image
 from .mask import mask_image
