e98169967ccf0b57949b2d82dbe8bcd95bb1f246
Vita Zhang <vita.zhang@duke.edu>
2024-08-23 14:45:35 -0400

    fix syntax in featurize.py

--- src/condensate_analysis/featurize.py
+++ src/condensate_analysis/featurize.py
@@ -6,7 +6,7 @@ def featurize(img, mask, features="default"):
         features = ['label', 'slice', 'area', 'centroid', 'mean_intensity', 'eccentricity']
     # Need way to split builtin and custom features. Loop over and decide whether to use custom.
     
-    feat_table = pd.DataFrame(ski.measure.regionprops_table(mask, intensity_image=img
+    feat_table = pd.DataFrame(ski.measure.regionprops_table(mask, intensity_image=img,
                 properties=builtin_features, extra_properties=custom_features)
             ).set_index('label')
 
