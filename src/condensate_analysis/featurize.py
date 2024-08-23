3a4f48bb80b91f5eb63b090368c8c039dbef8e14
dinosaur <alex.w.crocker@gmail.com>
2024-08-23 14:03:17 -0400

    basic featurize fxn

--- src/condensate_analysis/featurize.py
+++ src/condensate_analysis/featurize.py
@@ -1,44 +1,15 @@
 import pandas as pd
-from skimage.measure import regionprops
+from skimage.measure import regionprops_table
 
-def featurize(img, mask):
-    regionprops = ski.measure.regionprops(mask, intensity_image=img)
-    supported_features_list = []
-    unsupported_features_list = []
-    #set parameters
-    # remove objects with area < area_threshold pixels
-    area_threshold=20
-
-    # list regionprops options for storing in info_table
-    for prop in regionprops[0]:
-        try:
-            regionprops[0][prop]
-            supported_features_list.append(prop)
-        except NotImplementedError:
-            unsupported_features_list.append(prop)
-
-        # print("Supported properties:")
-        # print("  " + "\n  ".join(supported_features_list))
-        # print()
-        # print("Unsupported properties:")
-        # print("  " + "\n  ".join(unsupported_features_list))
-
-        # create info table
-        df_info_table = pd.DataFrame(ski.measure.regionprops_table(labels, prt_MAX_bs1,
-                properties=['label', 'slice', 'area', 'centroid', 'mean_intensity', 'eccentricity'])
+def featurize(img, mask, features="default"):
+    if features == "default":
+        features = ['label', 'slice', 'area', 'centroid', 'mean_intensity', 'eccentricity']
+    # Need way to split builtin and custom features. Loop over and decide whether to use custom.
+    
+    feat_table = pd.DataFrame(ski.measure.regionprops_table(mask, intensity_image=img
+                properties=builtin_features, extra_properties=custom_features)
             ).set_index('label')
 
-        df_info_table = df_info_table.drop(df_info_table[df_info_table.area < area_threshold].index) 
-        df_info_table['area'] = df_info_table['area'].apply(lambda x: x*pxlsz_x**2) # rescale areas to µm^2
+    return feat_table
 
-        # create lists of integer-rounded x and y centroid positions
-        ypos = [int(round(elem, 0)) for elem in df_info_table["centroid-0"].tolist()]
-        xpos = [int(round(elem, 0)) for elem in df_info_table["centroid-1"].tolist()]
-
-        # plot rounded centroid positions over image
-        # plt.figure(figsize=(24, 8), tight_layout=True)
-        
-        # apply mask
-        # extract features
-        # return table
-    return features
+# Write custom features here
