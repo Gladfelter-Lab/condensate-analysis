10a2502e534096a9d3d2fc719cb72d59bd00d497
Vita Zhang <vita.zhang@duke.edu>
2024-08-28 14:41:53 -0400

    works now with stack options "protein/rna_max_project " and "protein/rna_brightest_frame", might need other options

--- src/condensate_analysis/mask.py
+++ src/condensate_analysis/mask.py
@@ -7,29 +7,40 @@ import numpy as np
 
 # Review dependencies, are all necessary?
 
-# Add option to specify whether to use FULLSTACK, MAXPROJ if stack is provided. 
+# Add option to specify whether to use FULLSTACK, MAXPROJ if stack is provided.
 # Write method for each.
 
-# Consult Wilton on whether to use single value or also allow other method to be specified. 
+# Consult Wilton on whether to use single value or also allow other method to be specified.
 
-# Replace "watershed" with "splitting" option, mulitple methods supported. What other methods? 
+# Replace "watershed" with "splitting" option, mulitple methods supported. What other methods?
 
-def mask_image(image, threshold="otsu", watershed=False, background_sub=False, clear_border=True):
+
+def mask_image(
+    image,
+    stack="protein_max_project",
+    threshold="otsu",
+    watershed=False,
+    background_sub=False,
+    clear_border=True,
+):
     """Create and return a 2D or 3D mask from an input image.
-    --- parameters --- 
+    --- parameters ---
     - image: ndarray of two or three dimensions
+    - stack: False, "protein_max_project","rna_max_project","protein_brightest_frame","rna_brightest_frame"
     - threshold: integer value specifying threshold or name of method to determine threshold ("otsu")
     - watershed: False or value specifying watershed footprint size.
     - background_sub: False or "median", which subtracts median pixel value, are currently supported
-    - clear_border: boolean for whether condensates touching image edges should be included in mask. 
+    - clear_border: boolean for whether condensates touching image edges should be included in mask.
         May need to use False with 3D images right now since blobs often touch top or bottom of Z-stack.
-    
+
     """
+    if stack:
+        image = _get_stack(image, stack)
     if background_sub:
         image = _apply_background(image, background_sub)
     thresh = _get_threshold(image, threshold)
     binary = image > thresh
-    # Need to replace clear_border option with new clear_border function capable only ONLY clearing XY border, not Z border. 
+    # Need to replace clear_border option with new clear_border function capable only ONLY clearing XY border, not Z border.
     if clear_border:
         binary = segmentation.clear_border(binary)
     if watershed:
@@ -38,22 +49,61 @@ def mask_image(image, threshold="otsu", watershed=False, background_sub=False, c
         mask = label(binary)
     return mask
 
+
+def _get_stack(image_array, stack):
+    protein = image_array[1, :]
+    rna = image_array[0, :]
+    # max projections
+    protein_max = np.max(protein, axis=0)
+    rna_max = np.max(rna, axis=0)
+    # get brightest single frames (based on protein or rna ch intensity)
+    #do we need other options such as (give rna channel where correponding protein channel is the brightest frame, part of Wil's code)
+    nframes = len(protein)
+    frame_mean_protein = []
+    frame_mean_rna = []
+    for frame in range(nframes):
+        frame_mean_protein.append(np.mean(protein[frame, :, :]))
+        frame_mean_rna.append(np.mean(rna[frame, :, :]))
+    protein_brightest_frame_index = frame_mean_protein.index(max(frame_mean_protein))
+    protein_brightest_frame = protein[protein_brightest_frame_index, :, :]
+    rna_brightest_frame_index = frame_mean_rna.index(max(frame_mean_rna))
+    rna_brightest_frame = rna[rna_brightest_frame_index, :, :]
+    if stack == "protein_max_project":
+        image_array = protein_max
+    elif stack == "rna_max_project":
+        image_array = rna_max
+    elif stack == "protein_brightest_frame":
+        image_array = protein_brightest_frame
+    elif stack == "rna_brightest_frame":
+        image_array = rna_brightest_frame
+    else:
+        print(
+            f"Warning: stack specified by user is not one of the supported stack, stack is not appplied."
+        )
+    return image_array
+
+
 def _get_threshold(image_array, method):
     if method == "otsu":
         thresh = threshold_otsu(image_array)
-    elif type(method)==int or type(method)==float:
+    elif type(method) == int or type(method) == float:
         thresh = method
     else:
-        print(f"Warning: threshold method specified by user is not one of the supported method, using deafult threshold method otsu method instead.")
+        print(
+            f"Warning: threshold method specified by user is not one of the supported method, using deafult threshold method otsu method instead."
+        )
         thresh = threshold_otsu(image_array)
     return thresh
 
+
 def _apply_watershed(binary, ws_size):
     distance = ndi.distance_transform_edt(binary)
-    distance = ndi.gaussian_filter(distance,5)
+    distance = ndi.gaussian_filter(distance, 5)
     # Consider allowing user defined watershed distance. watershed=value accepted as well as False (none) and True (auto)
     dims = len(binary.shape)
-    coords = peak_local_max(distance, footprint=np.ones([ws_size]*dims), labels=binary)
+    coords = peak_local_max(
+        distance, footprint=np.ones([ws_size] * dims), labels=binary
+    )
     mask = np.zeros(distance.shape, dtype=bool)
     print(mask.shape)
     mask[tuple(coords.T)] = True
@@ -62,8 +112,9 @@ def _apply_watershed(binary, ws_size):
     mask = label
     return labels
 
+
 def _apply_background(image, method):
-    if method=="median":
+    if method == "median":
         background = np.median(image)
         subtracted_image = image - background
     return subtracted_image
