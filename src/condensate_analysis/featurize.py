3a9204903ceafb0a241021869048aab4094cce0d
Vita Zhang <vita.zhang@duke.edu>
2024-08-28 15:32:52 -0400

    works now with splitting feature into builtin vs custom; write out option exist to write feature table to csv

--- src/condensate_analysis/featurize.py
+++ src/condensate_analysis/featurize.py
@@ -1,15 +1,67 @@
 import pandas as pd
-from skimage.measure import regionprops_table
+from skimage.measure import regionprops, regionprops_table
 
-def featurize(img, mask, features="default"):
+
+def featurize(img, mask, features="default", feature_table_output_path=""):
+    """generate feature table.
+    --- parameters ---
+    - img: img array, must be the same dimension as mask
+    - mask: value returned from mask_image
+    - features:"default" or list of features
+    - feature_table_output_path: False or output_path
+    """
+    # might need to add in stack option for which img stack to featurize
+
+    builtin_features = []
+    custom_features = []
     if features == "default":
-        features = ['label', 'slice', 'area', 'centroid', 'mean_intensity', 'eccentricity']
+        builtin_features = [
+            "label",
+            "slice",
+            "area",
+            "centroid",
+            "mean_intensity",
+            "eccentricity",
+        ]
     # Need way to split builtin and custom features. Loop over and decide whether to use custom.
-    
-    feat_table = pd.DataFrame(ski.measure.regionprops_table(mask, intensity_image=img,
-                properties=builtin_features, extra_properties=custom_features)
-            ).set_index('label')
+    else:
+        # vita: i think we have to run regionprops first in order to find out supported properties in order to split between built in and custom, but we can run it only if it's not default features.
+        supported_builtin_properties = []
+        # needs to define supported_custom_features
+        supported_custom_features = []
 
-    return feat_table
+        regionprops = regionprops(mask, intensity_image=img)
+        for prop in regionprops[0]:
+            try:
+                regionprops[0][prop]
+                supported_builtin_properties.append(prop)
+            except NotImplementedError:
+                pass
+        for feature in features:
+            if feature in supported_builtin_properties:
+                builtin_features.append(feature)
+            elif feature in supported_custom_features:
+                custom_features.append(feature)
+            else:
+                print(
+                    f"Warning: {feature} is not supported, and will not be generated."
+                )
+
+    feature_table = pd.DataFrame(
+        regionprops_table(
+            label_image=mask,
+            intensity_image=img,
+            properties=builtin_features,
+            extra_properties=custom_features,
+        )
+    ).set_index("label")
+
+    if feature_table_output_path:
+        feature_table.to_csv(
+            feature_table_output_path, header=True, index=False, mode="w"
+        )
+
+    return feature_table
+
 
 # Write custom features here
