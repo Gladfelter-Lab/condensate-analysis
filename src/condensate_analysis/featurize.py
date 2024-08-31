838e2ec4fd44ca0f817b2415c3dc5e1094b2fa4d
Vita Zhang <vita.zhang@duke.edu>
2024-08-31 17:03:03 -0400

    implemented feature to properties based on gm's idea,  try/except for scikit properties not supported for image type

--- src/condensate_analysis/featurize.py
+++ src/condensate_analysis/featurize.py
@@ -2,105 +2,130 @@ import pandas as pd
 import numpy as np
 from skimage.measure import regionprops, regionprops_table
 
+dictionary_feature_to_properties = {
+    "default": ["label", "slice", "area", "centroid", "mean_intensity", "eccentricity"],
+    "size": ["area", "num_pixels"],
+    "shape": ["centroid", "axis_major_length"],
+    "area": ["area", "area_bbox", "area_convex", "area_filled"],
+    "axis": ["axis_major_length", "axis_minor_length"],
+    "centroid": [
+        "centroid",
+        "centroid_local",
+        "centroid_weighted",
+        "centroid_weighted_local",
+    ],
+    "coords": ["coords", "coords_scaled"],
+    "image": ["image", "image_convex", "image_filled", "image_intensity"],
+    "inertia": ["inertia_tensor", "inertia_tensor_eigvals"],
+    "intensity": [
+        "image_intensity",
+        "inertia_tensor",
+        "inertia_tensor_eigvals",
+        "intensity_max",
+        "intensity_mean",
+        "intensity_min",
+        "intensity_std",
+    ],
+    "moments": [
+        "moments",
+        "moments_central",
+        "moments_hu",
+        "moments_normalized",
+        "moments_weighted",
+        "moments_weighted_central",
+        "moments_weighted_hu",
+        "moments_weighted_normalized",
+    ],
+    "misc": [],
+    "scikit_all": [
+        "area",
+        "area_bbox",
+        "area_convex",
+        "area_filled",
+        "axis_major_length",
+        "axis_minor_length",
+        "bbox",
+        "centroid",
+        "centroid_local",
+        "centroid_weighted",
+        "centroid_weighted_local",
+        "coords",
+        "coords_scaled",
+        "eccentricity",
+        "equivalent_diameter_area",
+        "euler_number",
+        "extent",
+        "feret_diameter_max",
+        "image",
+        "image_convex",
+        "image_filled",
+        "image_intensity",
+        "inertia_tensor",
+        "inertia_tensor_eigvals",
+        "intensity_max",
+        "intensity_mean",
+        "intensity_min",
+        "intensity_std",
+        "label",
+        "moments",
+        "moments_central",
+        "moments_hu",
+        "moments_normalized",
+        "moments_weighted",
+        "moments_weighted_central",
+        "moments_weighted_hu",
+        "moments_weighted_normalized",
+        "num_pixels",
+        "orientation",
+        "perimeter",
+        "perimeter_crofton",
+        "slice",
+        "solidity",
+    ],
+    "custom_all": ["intensity_total"],
+}
 
-def featurize(img, mask, feature_options, export_path=""):
-    ### list of built-in properties: https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.regionprops
-    
-    ### features that are always included:
-    properties = ['label']
-    extra_properties = []
-    
-    ### features related to size:
-    if feature_options['size']:
-        properties = properties + ['area', 'num_pixels']
-    
-    ### features related to intensity:
-    if feature_options['intensity']:
-        properties = properties + ['max_intensity', 'mean_intensity', 'min_intensity']
+def featurize(img, mask, features, feature_table_output_path=""):
+    properties = ["label"]  # always want to include label
+    for feature in features:
+        properties += dictionary_feature_to_properties[feature]
+    # partition properties into built-into-scikit vs custom
+    scikit_properties = []
+    custom_properties = []
+    for property in properties:
+        if property in dictionary_feature_to_properties["scikit_all"]:
+            scikit_properties.append(property)
+        elif property in dictionary_feature_to_properties["custom_all"]:
+            function = dictionary_custom_property_to_function[property]
+            custom_properties.append(function)
+        else:
+            print(
+                f"Warning:{property} is not supported by scikit-image or custom, removing from table."
+            )
 
-        def total_intensity(region, intensities): #we could leave smaller functions inside and define larger ones
-            return np.sum(intensities[region])    #down below, or we could move all of them below
-        
-        def std_intensity(region, intensities): 
-            return np.std(intensities[region])
-        
-        extra_properties.append(total_intensity)
-        extra_properties.append(std_intensity)
-    
-    ### features related to shape:
-    if feature_options['shape']:
-        properties = properties + ['centroid', 'axis_major_length']
+    try:
+        feature_table = pd.DataFrame(
+            regionprops_table(
+                mask,
+                img,
+                properties=scikit_properties,
+                extra_properties=custom_properties,
+            )
+        )
+    except AttributeError as e:
+        raise NotImplementedError(f"{e.name} is not supported for this image type")
 
-    ### creating table of features:
-    feature_table = pd.DataFrame(regionprops_table(mask, img, properties=properties, 
-                                                   extra_properties=extra_properties))
-
-    ### exporting csv features to specified loccation:
-    if export_path:
-        feature_table.to_csv(export_path, header=True, index=False, mode="w")
-
+    if feature_table_output_path:
+        feature_table.to_csv(
+            feature_table_output_path, header=True, index=False, mode="w"
+        )
     return feature_table
-    
-### gm: commenting out previous:   
-    # """generate feature table.
-    # --- parameters ---
-    # - img: img array, must be the same dimension as mask
-    # - mask: value returned from mask_image
-    # - features:"default" or list of features
-    # - feature_table_output_path: False or output_path
-    # """
-    # # might need to add in stack option for which img stack to featurize
-
-    # builtin_features = []
-    # custom_features = []
-    # if features == "default":
-    #     builtin_features = [
-    #         "label",
-    #         "slice",
-    #         "area",
-    #         "centroid",
-    #         "mean_intensity",
-    #         "eccentricity",
-    #     ]
-    # # Need way to split builtin and custom features. Loop over and decide whether to use custom.
-    # else:
-    #     # vita: i think we have to run regionprops first in order to find out supported properties in order to split between built in and custom, but we can run it only if it's not default features.
-    #     supported_builtin_properties = []
-    #     # needs to define supported_custom_features
-    #     supported_custom_features = []
-
-    #     regionprops = regionprops(mask, intensity_image=img)
-    #     for prop in regionprops[0]:
-    #         try:
-    #             regionprops[0][prop]
-    #             supported_builtin_properties.append(prop)
-    #         except NotImplementedError:
-    #             pass
-    #     for feature in features:
-    #         if feature in supported_builtin_properties:
-    #             builtin_features.append(feature)
-    #         elif feature in supported_custom_features:
-    #             custom_features.append(feature)
-    #         else:
-    #             print(
-    #                 f"Warning: {feature} is not supported, and will not be generated."
-    #             )
-
-    # feature_table = pd.DataFrame(
-    #     regionprops_table(
-    #         label_image=mask,
-    #         intensity_image=img,
-    #         properties=builtin_features,
-    #         extra_properties=custom_features,
-    #     )
-    # ).set_index("label")
-
-    # if feature_table_output_path:
-    #     feature_table.to_csv(
-    #         feature_table_output_path, header=True, index=False, mode="w"
-    #     )
-
-    # return feature_table
 
 
 # Write custom features here
+def intensity_total(region, intensities):
+    return np.sum(intensities[region])
+
+
+# vz:can turn this into a class/decorator situation so that the dictionary is automatically generated when developer or user add new custom property
+dictionary_custom_property_to_function = {"intensity_total": intensity_total}
