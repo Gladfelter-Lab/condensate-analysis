ab40d5345ae7f8888a2e18374fe3df5c6b1cb62b
dinosaur <alex.w.crocker@gmail.com>
2024-10-02 10:54:24 -0400

    attemped to fix var not found error

--- src/condensate_analysis/featurize.py
+++ src/condensate_analysis/featurize.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from skimage.measure import regionprops, regionprops_table
-import feature_dict
+from feature_dict import *
 
 def featurize_image(img, mask, features=["default"], custom_features=None, feature_table_output_path=False):
     """
