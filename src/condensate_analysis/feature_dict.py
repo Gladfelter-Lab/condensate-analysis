c2782605cdcb90631b56bae6eab64fb7bf3f28a7
sierrajc598 <sierrajc598@gmail.com>
2024-10-31 11:20:11 -0400

    Update feature_dict.py

--- src/condensate_analysis/feature_dict.py
+++ src/condensate_analysis/feature_dict.py
@@ -87,23 +87,47 @@ dictionary_feature_to_properties = {
 # Write custom features here
 def intensity_total(region, intensities):
     return np.sum(intensities[region])
+    
 def centroid_info(mask, img):
+    # Get the centroid coordinates
     point = ndimage.center_of_mass(mask)
-    round_point = [ int(round(elem, 0)) for elem in point ]
-    if len(round_point) == 2 : 
-        intensity = int(img[round_point[0], round_point[1]])
-                       #img[round_point[0]+1, round_point[1]] +
-                       #img[round_point[0], round_point[1]+1]+
-                       #img[round_point[0]-1, round_point[1]]+
-                       #img[round_point[0], round_point[1]-1]/ 5)
+    round_point = [int(round(elem, 0)) for elem in point]
+    
+    # Define the dimensions of the image
+    img_shape = img.shape
+    
+    def is_within_bounds(coords):
+        """Check if the given coordinates are within the image bounds."""
+        return all(0 <= coords[i] < img_shape[i] for i in range(len(coords)))
+    
+    # Handle 2D images
+    if len(round_point) == 2:
+        neighbors = [
+            (0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)
+        ]
+        intensity_values = []
+        for dx, dy in neighbors:
+            new_point = (round_point[0] + dx, round_point[1] + dy)
+            if is_within_bounds(new_point):
+                intensity_values.append(img[new_point[0], new_point[1]])
+        
+        intensity = int(sum(intensity_values) / len(intensity_values)) if intensity_values else 0
+    
+    # Handle 3D images
     elif len(round_point) == 3:
-        intensity = int(img[round_point[0], round_point[1], round_point[2]])
-                       #img[round_point[0], round_point[1]+1, round_point[2]] +
-                       #img[round_point[0], round_point[1]-1, round_point[2]] +
-                       #img[round_point[0], round_point[1], round_point[2]+1] +
-                       #img[round_point[0], round_point[1], round_point[2]-1]/5)
+        neighbors = [
+            (0, 0, 0), (1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0),
+            (0, 0, 1), (0, 0, -1)
+        ]
+        intensity_values = []
+        for dx, dy, dz in neighbors:
+            new_point = (round_point[0] + dx, round_point[1] + dy, round_point[2] + dz)
+            if is_within_bounds(new_point):
+                intensity_values.append(img[new_point[0], new_point[1], new_point[2]])
+        
+        intensity = int(sum(intensity_values) / len(intensity_values)) if intensity_values else 0
+    
     return intensity
-
 
 # vz:can turn this into a class/decorator situation so that the dictionary is automatically generated when developer or user add new custom property
 dictionary_custom_property_to_function = {"intensity_total": intensity_total, "centroid_info":centroid_info}
